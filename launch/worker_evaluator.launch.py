25a404b160ac6c80be74bd223abed459a7f8e4de
Nao Takahashi <s24c1073hs@s.chibakoudai.jp>
2025-12-30 10:53:38 +0900

    Update launch file

--- launch/worker_evaluator.launch.py
+++ launch/worker_evaluator.launch.py
@@ -4,20 +4,19 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
     skip_probability = LaunchConfiguration('skip_probability')
 
     return LaunchDescription([
-
         DeclareLaunchArgument(
             'skip_probability',
             default_value='0.0',
-            description='probability to skip publish'
+            description='Probability to skip publishing'
         ),
 
         Node(
             package='mypkg',
             executable='worker',
+            emulate_tty=True,   # ← worker にも付けるとより確実
             parameters=[
                 {'publish_rate': 1.0},
                 {'skip_probability': skip_probability},
@@ -27,6 +26,7 @@ def generate_launch_description():
         Node(
             package='mypkg',
             executable='evaluator',
+            emulate_tty=True,
             parameters=[
                 {'ok_time': 1.5},
                 {'warn_time': 3.0},
