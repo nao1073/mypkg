e4ab0cbd54e96d73799e75d568bcd786440e3fa9
Nao Takahashi <s24c1073hs@s.chibakoudai.jp>
2026-01-05 23:14:20 +0900

    Update test

--- test/test.bash
+++ test/test.bash
@@ -1,20 +1,23 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2025 Nao Takahashi
 # SPDX-License-Identifier: BSD-3-Clause
+
 dir=~
+
 [ "$1" != "" ] && dir="$1"
 
 cd "$dir/ros2_ws" || exit 1
 colcon build
 source "$dir/.bashrc"
 
-timeout 30 ros2 launch mypkg worker_evaluator.launch.py skip_probability:=0.0 > /tmp/mypkg.log 2>&1
+timeout 60 ros2 launch mypkg worker_evaluator.launch.py skip_probability:=0.0 > /tmp/mypkg.log 2>&1
 
-cat /tmp/mypkg.log | grep 'EXCELLENT'
+cat /tmp/mypkg.log | grep 'EXCELLENT' || exit 1
 
-timeout 30 ros2 launch mypkg worker_evaluator.launch.py skip_probability:=0.4 > /tmp/mypkg.log 2>&1
-cat /tmp/mypkg.log | grep 'WARNING'
+timeout 60 ros2 launch mypkg worker_evaluator.launch.py skip_probability:=0.4 > /tmp/mypkg.log 2>&1
+cat /tmp/mypkg.log | grep 'WARNING' || exit 1
 
-timeout 30 ros2 launch mypkg worker_evaluator.launch.py skip_probability:=0.8 > /tmp/mypkg.log 2>&1
-cat /tmp/mypkg.log | grep 'CRITICAL'
+timeout 60 ros2 launch mypkg worker_evaluator.launch.py skip_probability:=0.8 > /tmp/mypkg.log 2>&1
+cat /tmp/mypkg.log | grep 'CRITICAL' || exit 1
 
+exit 0
